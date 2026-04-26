23571e106fd86f1e7b5cbcf4f43c146feb3fcd51
James Jasper Fadden O'Roarke <faddenjasper@gmail.com>
2026-04-26 15:05:01 -0700

    camera movement with arrowkeys

--- camera_rig.gd
+++ camera_rig.gd
@@ -4,3 +4,6 @@ extends Node3D
 
 func _process(delta):
 	global_position = target.global_position
+	
+	if Input.is_action_pressed("camera_left"): rotate_y(0.05)
+	if Input.is_action_pressed("camera_right"): rotate_y(-0.05)
